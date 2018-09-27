e10791cc26c675783db0982975bcfdb6d34d9250
Mohammad Rajaul Islam <springapidev@gmail.com>
2018-09-27 13:40:53 +0600

    jhsdfh

---
+++ CoreJavaEvidencePractice/src/com/coderbd/MCQ/ArrayEx.java
@@ -0,0 +1,15 @@
+package com.coderbd.MCQ;
+
+public class ArrayEx {
+
+    public static void main(String[] args) {
+        double[] price = new double[]{5, 6, 8};
+        System.out.println(price[1]);
+        byte b;
+        int x = 258;
+        b = (byte) x;
+        byte c = 127;
+        int y = Byte.MAX_VALUE;
+        System.out.println(y);
+    }
+}
