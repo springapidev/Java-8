aa5856c0265642f01e3bc86167735b7a3e795293
Mohammad Rajaul Islam <springapidev@gmail.com>
2018-09-27 11:13:35 +0600

    evidence suggestions dd

---
+++ CoreJavaEvidencePractice/src/com/coderbd/questions/PlusPlusMinusMinus.java
@@ -0,0 +1,12 @@
+package com.coderbd.questions;
+
+public class PlusPlusMinusMinus {
+
+    public static void main(String[] args) {
+        int x = 2;
+        int y = 5;
+        // System.out.println(x++ + "" + ++y);
+        System.out.println(++x + " " + y--);
+        System.out.println(x + " " + y);
+    }
+}
